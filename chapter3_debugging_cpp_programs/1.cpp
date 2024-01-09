49ab5e5ace295aff26e086190c6a4ccce3143343
YuZhang <zhangyu_geoboy@163.com>
2024-01-09 13:01:21 +0800

    chapter3_4

--- chapter3_debugging_cpp_programs/1.cpp
+++ chapter3_debugging_cpp_programs/1.cpp
@@ -8,6 +8,19 @@
 /*
 1. Finding problems via code inspection
 2. Finding problems by running the problem
+3. Reproducing the problem
+*/
+
+// 3.4 Basic debugging tactics
+/*
+1. Commenting out your code
+2. Validating your code flow
+(1). It can be helpful to place statements at the top of your functions to 
+print the function’s name. That way, when the program runs, you can see which functions are getting called.
+(2). When printing information for debugging purposes, use std::cerr instead of std::cout. 
+std::cerr is unbuffered, which means anything you send to it will output immediately.
+3. Printing values 
+4. Why using printing statements to debug isn't great
 */
 
 int main(){
