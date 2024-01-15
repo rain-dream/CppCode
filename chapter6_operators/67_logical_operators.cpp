bfcbc0860dfebd89db4ba9433c6bed7cc9403742
zy <zhangyu_geoboy@163.com>
2024-01-15 11:21:47 +0800

    2024.1.15_1

---
+++ chapter6_operators/67_logical_operators.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+// 6.7 Logical operators
+/*
+1. Logical NOT
+
+2. Logical OR
+
+3. Logical AND
+
+4. Short circuit evaluation
+(1). If the left operand evaluates to false, logical AND knows it must return false regardless of whether the right operand evaluates to true or false. 
+In this case, the logical AND operator will go ahead and return false immediately without even evaluating the right operand! This is known as short circuit evaluation, 
+and it is done primarily for optimization purposes.
+(2). Short circuit evaluation presents another opportunity to show why operators that cause side effects should not be used in compound expressions.
+*/
+
+int main(){
+
+    return 0;
+}
